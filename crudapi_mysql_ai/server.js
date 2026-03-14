db63b166e4e7b8d7efc86e9203444f2bb4cd8068
ctrljerome <torresjeromed@gmail.com>
2026-03-14 12:33:40 +0800

    fix logging api

--- crudapi_mysql_ai/server.js
+++ crudapi_mysql_ai/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import moodRoutes from "./routes/moods.js";
-import { db } from "/db.js"
+import { db } from "./db.js"
 
 dotenv.config();
 
